80734d814cdacc85e4d3aab5136543cabb8ed220
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2021-11-25 17:01:35 -0500

    Test

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-GITHUB_BASE_URL="https://github.com/TypicalTropic/pterodactyl-installer-script/blob/main/"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/TypicalTropic/pterodactyl-installer-script/main/"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
