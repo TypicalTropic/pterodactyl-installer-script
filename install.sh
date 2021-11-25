752114b23cbb176ea1c5127d01e79cbb4402d7bd
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2021-11-25 17:03:34 -0500

    Added blob to url

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-GITHUB_BASE_URL="https://raw.githubusercontent.com/TypicalTropic/pterodactyl-installer-script/main/"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/TypicalTropic/pterodactyl-installer-script/blob/main/"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
