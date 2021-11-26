9debe9f8ce2d2df4dade3224a6f7c01000cf6ce6
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2021-11-26 18:12:27 -0500

    Uodated test Script

--- test-script.sh
+++ test-script.sh
@@ -16,7 +16,47 @@ detect_distro() {
   OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
 }
 
+check_os_comp() {
+  CPU_ARCHITECTURE=$(uname -m)
+  if [ "${CPU_ARCHITECTURE}" != "x86_64" ]; then # check the architecture
+    print_warning "Detected CPU architecture $CPU_ARCHITECTURE"
+    print_warning "Using any other architecture than 64 bit (x86_64) May cause problems."
+
+    echo -e -n "* Are you sure you want to proceed? (y/N):"
+    read -r choice
+
+    if [[ ! "$choice" =~ [Yy] ]]; then
+      print_error "Installation aborted!"
+      exit 1
+    fi
+  fi
+case "$OS" in
+  ubuntu)
+    PHP_SOCKET="/run/php/php8.0-fpm.sock"
+    [ "$OS_VER_MAJOR" == "18" ] && SUPPORTED=true
+    [ "$OS_VER_MAJOR" == "20" ] && SUPPORTED=true
+    [ "$OS_VER_MAJOR" == "2004" ] && SUPPORTED=true
+    ;;
+  *)
+    SUPPORTED=false
+    ;;
+  esac
+
+  # exit if not supported
+  if [ "$SUPPORTED" == true ]; then
+    echo "* $OS $OS_VER is supported."
+  else
+    echo "* $OS $OS_VER is not supported"
+    print_error "Unsupported OS"
+    exit 1
+  fi
+}
+
 detect_distro
 
 echo "$OS_VER_MAJOR"
+
+check_os_comp
+
+
 
