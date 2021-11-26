bc6d5f4c805c071775a1c3c6daf8eea0a99473c6
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2021-11-26 18:01:37 -0500

    Testing Script to detect os version

---
+++ test-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+detect_distro() {
+  if [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$(echo "$DISTRIB_ID" | awk '{print tolower($0)}')
+    OS_VER=$DISTRIB_RELEASE
+  elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS="debian"
+    OS_VER=$(cat /etc/debian_version)
+  fi
+
+  OS=$(echo "$OS" | awk '{print tolower($0)}')
+  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
+}
+
+echo "$OS_VER_MAJOR"
+
