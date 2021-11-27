30c106ba4d1b5c8d5acc8f8ce152c9ec7dc5a18f
Jordan McKoy <jordan.bmp.mckoy@gmail.com>
2021-11-26 19:30:06 -0500

    Updated Testing Script for Pteroq Service download

--- test-script.sh
+++ test-script.sh
@@ -1,62 +1,11 @@
 #!/bin/bash
 
-detect_distro() {
-  if [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$(echo "$DISTRIB_ID" | awk '{print tolower($0)}')
-    OS_VER=$DISTRIB_RELEASE
-  elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS="debian"
-    OS_VER=$(cat /etc/debian_version)
-  fi
+GITHUB_BASE_URL="https://typicaltropic.github.io/pterodactyl-installer-script/"
 
-  OS=$(echo "$OS" | awk '{print tolower($0)}')
-  OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
+install_pteroq() {
+  echo "* Installing pteroq service.."
+
+  curl -o /etc/systemd/system/pteroq.service $GITHUB_BASE_URL/configs/pteroq.service
 }
 
-check_os_comp() {
-  CPU_ARCHITECTURE=$(uname -m)
-  if [ "${CPU_ARCHITECTURE}" != "x86_64" ]; then # check the architecture
-    print_warning "Detected CPU architecture $CPU_ARCHITECTURE"
-    print_warning "Using any other architecture than 64 bit (x86_64) May cause problems."
-
-    echo -e -n "* Are you sure you want to proceed? (y/N):"
-    read -r choice
-
-    if [[ ! "$choice" =~ [Yy] ]]; then
-      print_error "Installation aborted!"
-      exit 1
-    fi
-  fi
-case "$OS" in
-  ubuntu)
-    PHP_SOCKET="/run/php/php8.0-fpm.sock"
-    [ "$OS_VER_MAJOR" == "18" ] && SUPPORTED=true
-    [ "$OS_VER_MAJOR" == "20" ] && SUPPORTED=true
-    [ "$OS_VER_MAJOR" == "2004" ] && SUPPORTED=true
-    ;;
-  *)
-    SUPPORTED=false
-    ;;
-  esac
-
-  # exit if not supported
-  if [ "$SUPPORTED" == true ]; then
-    echo "* $OS $OS_VER is supported."
-  else
-    echo "* $OS $OS_VER is not supported"
-    print_error "Unsupported OS"
-    exit 1
-  fi
-}
-
-detect_distro
-
-echo "$OS_VER_MAJOR"
-
-check_os_comp
-
-
-
+install_pteroq
